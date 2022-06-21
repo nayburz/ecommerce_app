2a67a703db6c355a440471827b28daf3c8500aa1
Adrian Neighbors <adrian102@gmail.com>
2022-06-21 15:41:29 -0700

    Removed image picker features

--- src/components/Authentication/SignUp.js
+++ src/components/Authentication/SignUp.js
@@ -7,12 +7,12 @@ import {
   TouchableOpacity,
   Image,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import Icon from "react-native-vector-icons/Ionicons";
 
 var { width } = Dimensions.get("window");
 
-export default function SignUpScreen({ navigation }) {
+export default function SignUp({ navigation }) {
   return (
     <View style={styles.container}>
       <View style={styles.loginHeader}>
