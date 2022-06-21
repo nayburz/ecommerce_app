774e5ceb82f1d47adf5bd55093e7174e7a95c2fc
Adrian Neighbors <adrian102@gmail.com>
2022-06-21 08:06:38 -0700

    Add loadUser function to app.js

--- App.js
+++ App.js
@@ -1,34 +1,37 @@
-import 'react-native-gesture-handler';
-import { Provider } from 'react-redux';
-import Store from './Redux/Store';
-import { NavigationContainer } from '@react-navigation/native';
+import "react-native-gesture-handler";
+import { Provider } from "react-redux";
+import Store from "./Redux/Store";
+import { NavigationContainer } from "@react-navigation/native";
 import Main from "./Navigations/Main";
-import Auth from "./Navigations/Auth"
+import { useDispatch, useSelector } from "react-redux";
+import Auth from "./Navigations/Auth";
+import { loadUser } from "./Redux/Actions/UserActions";
 
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { useState } from 'react';
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useEffect } from "react";
 
-
-export default function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(true)
+const App = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Provider store={Store}>
-        <NavigationContainer>
-
-          {
-            isAuthenticated ? (
-              <Main />
-            )
-              : (
-                <Auth />
-              )
-          }
-        </NavigationContainer>
+        <AppStack />
       </Provider>
     </SafeAreaView>
   );
-}
+};
 
+const AppStack = () => {
+  const { isAuthenticated, user } = useSelector((state) => state.user);
 
+  useEffect(() => {
+    Store.dispatch(loadUser());
+  }, []);
 
+  return (
+    <NavigationContainer>
+      <>{<>{isAuthenticated ? <Main /> : <Auth />}</>}</>
+    </NavigationContainer>
+  );
+};
+
+export default App;
--- src/components/Home/HomeProduct.js
+++ src/components/Home/HomeProduct.js
@@ -1,56 +1,46 @@
-import { StyleSheet, Text, View, Dimensions, ScrollView } from 'react-native'
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { getProduct } from '../../../Redux/Actions/ProductAction';
-import ProductCard from "../Home/ProductCard"
+import { StyleSheet, Text, View, Dimensions, ScrollView } from "react-native";
+import React, { useEffect } from "react";
 
-
+import { getProduct } from "../../../Redux/Actions/ProductAction";
+import ProductCard from "../Home/ProductCard";
 
 var { width } = Dimensions.get("window");
 
 export default function HomeProduct({ products }) {
+  return (
+    <View style={styles.container}>
+      <Text
+        style={{
+          fontSize: 25,
+          color: "#333",
+          textAlign: "center",
+        }}
+      >
+        Best Selling
+      </Text>
 
-    const { isAuthenticated, user } = useSelector((state) => state.user)
-
-
-    return (
-        <View style={styles.container}>
-            <Text
-                style={{
-                    fontSize: 25,
-                    color: "#333",
-                    textAlign: "center",
-                }}>
-                {isAuthenticated ? "okay" : "not"}
-            </Text>
-
-            <View style={styles.productCard}>
-                {products &&
-                    products.map(product => (
-                        <ProductCard
-                            key={product._id}
-                            product={product}
-                        />
-                    ))}
-            </View>
-
-        </View>
-    )
+      <View style={styles.productCard}>
+        {products &&
+          products.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        width: width,
-        padding: 10,
-        marginVertical: 10,
-        marginBottom: width / 6 - 5,
-    },
-    productCard: {
-        width: width * 1 - 10,
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-
-})
+  container: {
+    width: width,
+    padding: 10,
+    marginVertical: 10,
+    marginBottom: width / 6 - 5,
+  },
+  productCard: {
+    width: width * 1 - 10,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
